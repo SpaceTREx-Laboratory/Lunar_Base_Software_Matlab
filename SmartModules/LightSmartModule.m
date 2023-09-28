0ae13173a8abfcabb49d9d0a9f8def21facd2e62
mvillasana <mvillasana@email.arizona.edu>
2023-09-28 11:16:45 -0700

    Updated smart modules

--- SmartModules/LightSmartModule.m
+++ SmartModules/LightSmartModule.m
@@ -1,23 +1,42 @@
 classdef LightSmartModule < SmartModule
     properties
+        state;                  % State of the light
+        minTemp;
+        minDIst;                % Inclufe the distance threshold and temperature threshold?
     end
 
     methods
+        % Constructor
         function obj = LightSmartModule()
-            
         end
 
         % Methods %
-        function light = controlLight(obj, distance, temperature)
+        function cycle = cycleThroughSensors(obj)
+            for sensorNum = 1:length(obj.sensors)
+                if(processSignal(obj.sensors(sensorNum)))
+                    obj.state = 1;      % On
+                    return
+                end
+            end
+            obj.state = 0;              % No motion/temp detected
+        end
+        function smartLight = processSignal(obj, dataType, dataVal)
             % tempSensor takes temperature & sends to processor
             % proxSensor takes distance & sends to processor
             % processor takes temp data & sends signal to lightActuator
             % processor takes distance data & sends signal to lightActuator
-            
-            % if lightActuator.state == on
-            %   turn on the light
-            % else 
-            %   do nothing
+            if(strcmp(dataType, "Temperature") && dataVal >= obj.minTemp)
+                smartLight = 1;
+            elseif(strcmp(dataType, "Proximity") && dataVal <= obj.minDIst)
+                %Compare to distance
+                smartLight = 1;
+            else
+                smartLight = 0;
+            end
+        end
+
+        function illumination = UpdateGraphicsobj(obj)
+            % Update graphics
         end
     end
 end
--- SmartModules/SmartModule.m
+++ SmartModules/SmartModule.m
@@ -1,4 +1,4 @@
-classdef (Abstract) SmartModule < DataHandler
+classdef (Abstract) SmartModule < DataHandler & matlab.mixin.Heterogeneous
     properties
         sensors;                                                    % Array of sensors
         actuator;                                                   % Single actuator
@@ -8,25 +8,18 @@ classdef (Abstract) SmartModule < DataHandler
 
     % Abstract Methods %
     methods (Abstract)
-        signal = processSignal(obj)
+        signal = processSignal(obj, dataType)                                 % Remove redundancy of processor, every module should implement this class for processing
+        obj = UpdateGraphicsobj(obj)
     end
 
     methods
         % Constructor %
-        function obj = SmartLight(obj, sensors, actuator, processor)
+        function obj = SmartModule(obj, sensors, actuator, processor)
             obj.sensors = sensors;
             obj.actuator = actuator;
             obj.processor = processor;
             icon = obj.MIcon;
             obj.GraphicsHandle=imagesc('XData',(obj.Loc(1):obj.Loc(1)+size(icon,1))-(size(icon,1)/2),'YData',(obj.Loc(2):obj.Loc(2)+size(icon,2))-(size(icon,2)/2),'CData',icon,Parent=obj.Screen1Handle,Tag=obj.Tag);
-        end
-        % Methods %
-        function obj = UpdateGraphicsobj(obj)
-          icon=obj.InRIcon;
-          set(obj.GraphicsHandle,'XData',(obj.Loc(1):obj.Loc(1)+size(icon,1))-(size(icon,1)/2),'YData',(obj.Loc(2):obj.Loc(2)+size(icon,2))-(size(icon,2)/2),'CData',icon);
-        end
-
-        fu
-         
+        end       
     end
 end
